638489d5727166cca1ccf3262628d3b544442b9e
Louis Hermier <louis.hermier@proton.me>
2023-03-11 17:14:24 +0100

    move statuscode to response object, add actual function stub

--- implant/main.go
+++ implant/main.go
@@ -13,18 +13,21 @@ type command struct {
 }
 
 type response struct {
-	Status  bool   `json:"status"` // true if ok, false otherwise
-	Message string `json:"message"`
+	Status     bool   `json:"status"` // true if ok, false otherwise
+	StatusCode int    `json:"statuscode"`
+	Message    string `json:"message"`
 }
 
 var notFoundResponse response = response{ // can't const a struct yay
-	Status:  false,
-	Message: "URL or Method invalid",
+	Status:     false,
+	Message:    "URL or Method invalid",
+	StatusCode: http.StatusNotFound,
 }
 
 var pongResponse response = response{
-	Status:  true,
-	Message: "pong",
+	Status:     true,
+	Message:    "pong",
+	StatusCode: http.StatusOK,
 }
 
 func serverError(w http.ResponseWriter) {
@@ -33,16 +36,24 @@ func serverError(w http.ResponseWriter) {
 	w.Write([]byte("{}")) // be polite and still send actual JSON back
 }
 
-func sendResponse(res response, status int, w http.ResponseWriter) {
+func sendResponse(res response, w http.ResponseWriter) {
 	out, err := json.Marshal(res)
 	if err != nil {
 		log.Printf("Error serializing json!  %+v\n", res)
 		serverError(w)
 		return
 	}
-	w.WriteHeader(status)
+	w.WriteHeader(res.StatusCode)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(out))
+}
+
+func startCommand(r *http.Request) response {
+	return response{
+		Status:     false,
+		StatusCode: http.StatusNotImplemented,
+		Message:    "Function not yet implemented",
+	}
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
@@ -50,9 +61,11 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// handle requests on a one-by-one basis, there really arent that many...
 	// refactor to a switch or two for method and URL if this needs more than the actual endpoint and a test one
 	if r.Method == "GET" && r.URL.Path == "/ping" {
-		sendResponse(pongResponse, http.StatusOK, w)
+		sendResponse(pongResponse, w)
+	} else if r.Method == "POST" && r.URL.Path == "/run" {
+		sendResponse(startCommand(r), w)
 	} else {
-		sendResponse(notFoundResponse, http.StatusNotFound, w)
+		sendResponse(notFoundResponse, w)
 	}
 }
 
