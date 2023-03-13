9315e88542950178ee07fc1f2883e5249b33d323
Louis Hermier <louis.hermier@proton.me>
2023-03-13 20:02:41 +0100

    add minimal code to define modules to build

--- docker-builder/main.go
+++ docker-builder/main.go
@@ -1,1 +1,25 @@
 package main
+
+type module struct { // defined modules for installation in the docker file
+	gitrepo string   // git repo url
+	usegit  bool     // clone the git repo to build/install?
+	aptadds []string // dependencies to get through apt
+	build   []string // actual build commands outside of cloning and moving into the repo
+}
+
+func modules() map[string]module {
+	mods := make(map[string]module)
+	mods["rizin"] = module{
+		gitrepo: "https://github.com/rizinorg/rizin",
+		usegit:  true,
+		aptadds: []string{"meson", "ninja-build"},
+		build:   []string{"meson --buildtype=release build", "ninja -C build", "ninja -C build install"},
+	}
+	return mods
+}
+
+func main() {
+	image := "ubuntu:jammy"
+	mods := modules()
+
+}
