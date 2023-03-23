86101976d0fec52e8fab11cf3901497d58e9823e
Louis Hermier <louis.hermier@proton.me>
2023-03-23 12:49:17 +0100

    add a segments module

---
+++ modules/binanalysis/binsections.py
@@ -0,0 +1,15 @@
+import subprocess  # run commands
+import sys  # get arguments
+from base64 import b64decode  # decode arguments
+import json  # format output
+
+if len(sys.argv) != 2:
+    print("Wrong / Malformed arguments", file=sys.stderr)
+    sys.exit(1)
+else:
+    sample = b64decode(sys.argv[-1].encode('ascii')).decode('ascii')
+    # Get binary information in a JSON format from rz-bin
+    cmd = ["rz-bin", "-Sj", sample]
+    output = subprocess.run(cmd, stdout=subprocess.PIPE)
+    infobj = json.loads(output.stdout.decode())["sections"]
+    print(json.dumps(infobj), file=sys.stdout)
