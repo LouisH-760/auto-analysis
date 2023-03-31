b1f2b91039d43ee24d902db1d358b40b25ff5ce8
Nico Gruschke <14308103+NGruschke@users.noreply.github.com>
2023-03-31 19:29:30 +0200

    update npm module name and add dependencies for project

--- gui/src/main.ts
+++ gui/src/main.ts
@@ -4,6 +4,10 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
+
+import { loadFonts } from "@/plugins/webfontloader";
+import vuetify from "@/plugins/vuetify";
+loadFonts();
+app.use(vuetify).use(createPinia())
 
 app.mount('#app')
---
+++ gui/src/plugins/vuetify.ts
@@ -0,0 +1,28 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { md3 } from "vuetify/blueprints";
+
+// Composables
+import { createVuetify } from "vuetify";
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  blueprint: md3,
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: "#1867C0",
+          secondary: "#5CBBF6",
+        },
+      },
+    },
+  },
+});
