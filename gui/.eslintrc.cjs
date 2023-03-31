e566c6e721b3e76fecdd597a2e7e116fa5ca3571
Nico Gruschke <14308103+NGruschke@users.noreply.github.com>
2023-03-31 19:44:21 +0200

    fix dependeny issues

--- gui/.eslintrc.cjs
+++ gui/.eslintrc.cjs
@@ -1,15 +1,14 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+};
