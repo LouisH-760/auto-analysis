42b87c63eeba1237e29c089ac597d405e6604f8b
Nico Gruschke <14308103+NGruschke@users.noreply.github.com>
2023-03-31 12:15:44 +0200

    add volatility modules

---
+++ modules/memoryanalysis/volatility2/pstree.py
@@ -0,0 +1,16 @@
+import subprocess  # run commands
+import sys  # get arguments
+from base64 import b64decode  # decode arguments
+import json  # format output
+
+if len(sys.argv) != 3:
+    print("Wrong / Malformed arguments", file=sys.stderr)
+    sys.exit(1)
+else:
+    memorydump = b64decode(sys.argv[-2].encode('ascii')).decode('ascii')
+    profile = b64decode(sys.argv[-1].encode('ascii')).decode('ascii')
+    # Get process tree in a JSON format from volatility
+    cmd = ["volatility", "-f", memorydump, `--profile={0}`.format(profile), "pstree"]
+    output = subprocess.run(cmd, stdout=subprocess.PIPE)
+    imginfo = json.loads(output.stdout.decode())
+    print(json.dumps(imginfo), file=sys.stdout)
